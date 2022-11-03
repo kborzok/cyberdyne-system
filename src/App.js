7fb6a055b0a30862d8930c3c182b6971d2b6485e
kborzok <kborzok@gmail.com>
2022-11-03 09:54:56 +0100

    keresés befejezése

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import TerminatorList from "./components/terminator-list/terminator-list.componet";
 import { models } from "./models";
 import SearchBox from "./components/searchbox/searchbox.component";
 
-const App = () => {
-    return(
-        <div className="tc">
-            <h1>Terminátor modellek</h1>
-            <SearchBox />
-            <TerminatorList models={models} />
-        </div>
-
-    );
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            models: models,
+            searchfield: ''
+        }
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        //console.log(filteredModels);
+    }
+    render(){
+        const filteredModels = this.state.models.filter((model) => {
+            return(model.name
+            .toLowerCase()
+            .includes(this.state.searchfield.toLowerCase()) )
+        })
+        return(
+            <div className="tc">
+                <h1>Terminátor modellek</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <TerminatorList models={filteredModels} />
+            </div>
+        );
+    }
 }
+// const App = () => {
+//     return(
+//         <div className="tc">
+//             <h1>Terminátor modellek</h1>
+//             <SearchBox />
+//             <TerminatorList models={models} />
+//         </div>
+
+//     );
+// }
 export default App;
--- src/components/searchbox/searchbox.component.jsx
+++ src/components/searchbox/searchbox.component.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
     return(
         <div className="pa2">
             <input className="pa3 ba b--green bg-lightest-blue" 
-                type='search' placeholder='modell keresés...' />
+                type='search' placeholder='modell keresés...'
+                onChange={ searchChange } />
         </div>
-        
-
     );
 }
 
